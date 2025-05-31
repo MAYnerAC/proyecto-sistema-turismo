6cbf05ab4afed15a619f36fbeb60cb5c58fd095c
Mayner <ma2020067145@virtual.upt.pe>
2025-05-31 01:51:53 -0500

    Creacion del ProyectoSistemaTurismo.Tests

---
+++ ProyectoSistemaTurismo/ProyectoSistemaTurismo.Tests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("ProyectoSistemaTurismo.Tests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("ProyectoSistemaTurismo.Tests")]
+[assembly: AssemblyCopyright("Copyright ©  2025")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("cfb4a01c-6341-4253-84eb-f66361ca2426")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
