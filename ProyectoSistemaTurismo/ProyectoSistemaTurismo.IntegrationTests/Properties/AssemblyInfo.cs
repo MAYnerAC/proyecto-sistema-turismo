8d1dcd90284ecde4fd1df8c95bad3a30d1e4c3dc
Mayner <ma2020067145@virtual.upt.pe>
2025-06-07 11:53:30 -0500

    IntegrationTest - Crear nuevo proyecto de pruebas

---
+++ ProyectoSistemaTurismo/ProyectoSistemaTurismo.IntegrationTests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("ProyectoSistemaTurismo.IntegrationTests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("ProyectoSistemaTurismo.IntegrationTests")]
+[assembly: AssemblyCopyright("Copyright ©  2025")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("bfb89e06-76ec-4de7-9311-2d06c40648d9")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
